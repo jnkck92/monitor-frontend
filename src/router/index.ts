051cfba07ec90c6c730d6d134fb9a178dd45210b
jkueck <jkueck@eos-uptrade.de>
2026-09-03 11:45:52 +0200

    feature(slug): add slug for dashboard

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/d/:slug/',
       name: 'status',
       component: StatusView,
     },
